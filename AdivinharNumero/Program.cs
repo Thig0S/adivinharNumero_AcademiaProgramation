e8dd52e56e048b33432621da09fc2f95def99642
thig0S_ <thiagokovalski5@gmail.com>
2026-03-09 16:05:23 -0300

    validacao da entrada de usuario

--- AdivinharNumero/Program.cs
+++ AdivinharNumero/Program.cs
@@ -3,13 +3,30 @@ using System.Security.Cryptography;
 
 int numAleatorio = RandomNumberGenerator.GetInt32(1,101);
 int numeroUsuario = 0;
-int dificuldade = 0;
+// int dificuldade = 0;
 
 System.Console.WriteLine("--------------------");
 System.Console.WriteLine("Bem-vindo ao Jogo de adivinhacao!");
 System.Console.WriteLine("--------------------");
 
+while (true)
+{
 System.Console.Write("\nDigite um numero: ");
 numeroUsuario = Convert.ToInt32(Console.ReadLine());
 
-System.Console.WriteLine($"Numero aleatorio: {numAleatorio}");
+    if(numAleatorio == numeroUsuario)
+        {
+            System.Console.WriteLine("parabens voce acertou o numero!");
+            break;
+        }    
+    else if(numeroUsuario < numAleatorio)
+        {
+            System.Console.WriteLine("Numero digitado e MENOR!");
+        }
+    else
+        {
+            System.Console.WriteLine("Numero digitado e MAIOR");        
+        }
+}
+System.Console.WriteLine("Obrigado por jogar!");
+
