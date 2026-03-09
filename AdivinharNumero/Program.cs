ca2cb7fb7a3ff0046047bd78baf3ba5a888c1499
thig0S_ <thiagokovalski5@gmail.com>
2026-03-09 15:24:37 -0300

    V1

---
+++ AdivinharNumero/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Security.Cryptography;
+
+int numAleatorio = RandomNumberGenerator.GetInt32(1,101);
+int numeroUsuario = 0;
+int dificuldade = 0;
+
+System.Console.WriteLine("--------------------");
+System.Console.WriteLine("Bem-vindo ao Jogo de adivinhacao!");
+System.Console.WriteLine("--------------------");
+
+System.Console.Write("\nDigite um numero: ");
+numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Numero aleatorio: {numAleatorio}");
